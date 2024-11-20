5020d1212cc3cec4e2c604bb05333cafd61e1d74
luminousbinary <boolwrld@outlook.com>
2024-11-20 15:02:12 +0000

    hugr changes img

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ export default {
           // an ideal pay 
           // our web- social media
           // supervising, design
+        }, 
+        slideUp: {
+          "0%": { transform: "translateY(100%)", opacity:"0" },
+          "100%": { transform: "translateY(0%)", opacity:"1" },
+          // dream 
+          //  if you were to manage you time: 2days a week: how much
+          // an ideal pay 
+          // our web- social media
+          // supervising, design
         },
         scroll: {
           "0%": { transform: "translateX(0%)" },
@@ -20,7 +29,8 @@ export default {
       },
       animation: {
         scroll: "scroll 40s linear infinite",
-        slideIn:"slideIn 3s ease-in-out"
+        slideIn:"slideIn 3s ease-in-out",
+        slideUp:"slideUp 3s ease-in-out",
       },
       fontFamily: {
         grotesk: ["CabinetGrotesk", "sans-serif"],

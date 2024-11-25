75c37bc1d0155dbc15b97b92821b4d46dbf49887
luminousbinary <boolwrld@outlook.com>
2024-11-25 10:22:44 +0000

    slight changers

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,39 @@ const RevealOnScroll: React.FC<RevealOnScrollProps> = ({ children, animationClas
   );
 };
 
+const RevealOnScrollSlide: React.FC<RevealOnScrollProps> = ({ children, animationClasses = "opacity-100 transition-transform duration-300 ease-in-out animate-slideIn " }) => {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const scrollObserver = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          if (ref.current) scrollObserver.unobserve(ref.current);
+        }
+      },
+      { threshold: 0.2 } // Trigger when 20% of the element is visible
+    );
+
+    if (ref.current) scrollObserver.observe(ref.current);
+
+    return () => {
+      if (ref.current) scrollObserver.unobserve(ref.current);
+    };
+  }, []);
+
+  const classes = `transition-all duration-1000 transform 
+        ${isVisible ? animationClasses : "opacity-0 translate-x-10"}`;
+
+  return (
+    <div ref={ref} className={classes}>
+      {children}
+    </div>
+  );
+};
+
 function App() {
   const dummy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const dummy2 = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -52,14 +85,14 @@ function App() {
           <div className="logo max-w-[124.76px] "><img src="images/Logo.svg" alt="ARME Logo" /></div>
 
           <div className="links hidden md:gap-4 md:flex">
-            <button className='flex text-sm font-semibold text-nowrap justify-center items-center gap-4 text-white bg-black px-3 rounded-full w-[156px] h-[53px]'>
-              Join Us  <a href="https://x.com/xarmebot"> <img src="images/inverse/telegram.svg" width={"20px"} alt="telegram" /></a>
-              <a href=""><img src="images/inverse/x.svg" width={"20px"} alt="X (formerly Twitrter)" /></a>
+            <button className='flex text-sm font-semibold text-nowrap justify-center items-center gap-4 text-white bg-black px-3 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out w-[156px] h-[53px]'>
+              Join Us  <a href="https://x.com/xarmebot" className='hover:animate-hover'> <img src="images/inverse/telegram.svg" width={"20px"} alt="telegram" /></a>
+              <a href="" className='hover:animate-hover'><img src="images/inverse/x.svg" width={"20px"} alt="X (formerly Twitrter)" /></a>
             </button>
-            <button className='flex text-sm font-semibold justify-center items-center gap-4 text-white bg-black px-3 rounded-full w-[126px] h-[53px]'>
+            <button className='flex text-sm font-semibold justify-center items-center gap-4 text-white bg-black px-3 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 ease-in-out w-[126px] h-[53px]'>
               Git
-              <a href="https://x-arme-collective.gitbook.io">  <img src="images/inverse/GitBook - Icon - Light 1.svg" width={"20px"} alt="gitbook" /></a>
-              <a href="https://github.com/XARME-COLLECTIVE/">  <img src="images/inverse/github.svg" width={"20px"} alt="github" /></a>
+              <a href="https://x-arme-collective.gitbook.io" className='hover:animate-hover'>  <img src="images/inverse/GitBook - Icon - Light 1.svg" width={"20px"} alt="gitbook" /></a>
+              <a href="https://github.com/XARME-COLLECTIVE/" className='hover:animate-hover'>  <img src="images/inverse/github.svg" width={"20px"} alt="github" /></a>
             </button>
           </div>
 
@@ -128,7 +161,7 @@ function App() {
 
 
               </div>
-              <button className=' w-[247px] md:w-[310px] h-[54px] md:h-[66px] font-medium md:text-lg text-sm '>And More...</button>
+              <a href=""><button className=' w-[247px] md:w-[310px] h-[54px] md:h-[66px] font-medium md:text-lg text-sm hover:scale-110 transition-transform duration-300 ease-in-out'>And More...</button></a>
 
             </div>
           </section>
@@ -148,35 +181,35 @@ function App() {
             </div>
 
             <div className="imagelist flex flex-col md:flex-row md:flex-wrap pt-[70px] md:pt-[153px]   ">
-              <div className="imageitem animate-slideUp md:flex flex-col md:mb-[-70px] justify-end w-[360px] basis-1/3 ">
+              <div className="imageitem animate-slideUp md:flex flex-col md:mb-[-70px] justify-end w-[360px] basis-1/3  ">
                 <div className="lil-title">
-                  <img src="images/xot/1f.png" alt="image1" />
+                  <img className='transition-transform duration-300 ease-in-out hover:animate-hoverL' src="images/xot/1f.png" alt="image1" />
                 </div>
               </div>
 
               <div className="imageitem animate-slideUp md:flex flex-col mt-[-51px] md:mt-[0px] md:mb-[70px] w-[360px] basis-1/3 ">
                 <div className="lil-title">
-                  <img src="images/xot/6f.png" alt="image1" />
+                  <img className='transition-transform duration-300 ease-in-out hover:animate-hoverL' src="images/xot/6f.png" alt="image1" />
                 </div>
               </div>
               <div className="imageitem animate-slideUp md:flex flex-col mt-[-51px] md:mt-[0px] md:mb-[-100px] justify-end w-[360px] basis-1/3 ">
                 <div className="lil-title">
-                  <img src="images/xot/3f.png" alt="image1" />
+                  <img className='transition-transform duration-300 ease-in-out hover:animate-hoverL' src="images/xot/3f.png" alt="image1" />
                 </div>
               </div>
               <div className="imageitem animate-slideUp md:flex flex-col mt-[-51px] md:mt-[0px] md:justify-center w-[360px] basis-1/3 ">
                 <div className="lil-title">
-                  <img src="images/xot/5f.png" alt="image1" />
+                  <img className='transition-transform duration-300 ease-in-out hover:animate-hoverL' src="images/xot/5f.png" alt="image1" />
                 </div>
               </div>
               <div className="imageitem animate-slideUp md:flex flex-col mt-[-51px] md:mt-[-70px] w-[360px] basis-1/3 ">
                 <div className="lil-title">
-                  <img src="images/xot/4f.png" alt="image1" />
+                  <img className='transition-transform duration-300 ease-in-out hover:animate-hoverL' src="images/xot/4f.png" alt="image1" />
                 </div>
               </div>
               <div className="imageitem animate-slideUp md:flex flex-col mt-[-51px] md:mt-[0px] md:justify-center w-[360px] basis-1/3 ">
                 <div className="lil-title">
-                  <img src="images/xot/2f.png" alt="image1" />
+                  <img className='transition-transform duration-300 ease-in-out hover:animate-hoverL' src="images/xot/2f.png" alt="image1" />
                 </div>
               </div>
 
@@ -246,30 +279,43 @@ function App() {
             </div>
 
             <div className="li bg-[#F9FAF9]  basis-9/12 px-[20px] md:px-[110px] ">
-              <div className="liitems animate-slideIn flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-end items-center ml-[66px] ">
-                <span className='bg-[#9DC4F5] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1  rounded-[100%] text-[10px] md:text-[14px]'>500m</span>
-                500 million posts per day
-              </div>
-              <div className="liitems animate-slideIn flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-center items-center ml-[36px] ">
-                <span className='bg-[#9DC4F5] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>100b</span>
-                100 billion+ impressions per day
-              </div>
-              <div className="liitems animate-slideIn flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tighter font-medium justify-start items-center ml-0 ">
-                <span className='bg-[#9DC4F5] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>250k</span>
-                250,000 posts with atleast 1m <br className="hidden md:inline" /> impressions per day
-              </div>
-              <div className="liitems animate-slideIn flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-start items-center ml-[23px] ">
-                <span className='bg-[#FF5A4D] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>1.5m</span>
-                1.5 million new <br className="inline md:hidden" />  signups everyday
-              </div>
-              <div className="liitems animate-slideIn flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-center items-center ml-[36px] ">
-                <span className='bg-[#FF5A4D] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>14%</span>
-                Users spending 14% more time <br className="hidden md:inline" /> on X
-              </div>
-              <div className="liitems animate-slideIn flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-end items-center ml-[66px]">
-                <span className='bg-[#FBE74E] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>No.1</span>
-                The number 1 news channel <br className="hidden md:inline" /> on Earth
-              </div>
+
+              <RevealOnScrollSlide>
+                <div className="liitems hover:animate-everScale flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-end items-center ml-[66px] ">
+                  <span className='bg-[#9DC4F5] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1  rounded-[100%] text-[10px] md:text-[14px]'>500m</span>
+                  500 million posts per day
+                </div>
+              </RevealOnScrollSlide>
+              <RevealOnScrollSlide>
+                <div className="liitems hover:animate-everScale flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-center items-center ml-[36px] ">
+                  <span className='bg-[#9DC4F5] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>100b</span>
+                  100 billion+ impressions per day
+                </div>
+              </RevealOnScrollSlide>
+              <RevealOnScrollSlide>
+                <div className="liitems hover:animate-everScale flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tighter font-medium justify-start items-center ml-0 ">
+                  <span className='bg-[#9DC4F5] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>250k</span>
+                  250,000 posts with atleast 1m <br className="hidden md:inline" /> impressions per day
+                </div>
+              </RevealOnScrollSlide>
+              <RevealOnScrollSlide>
+                <div className="liitems hover:animate-everScale flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-start items-center ml-[23px] ">
+                  <span className='bg-[#FF5A4D] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>1.5m</span>
+                  1.5 million new <br className="inline md:hidden" />  signups everyday
+                </div>
+              </RevealOnScrollSlide>
+              <RevealOnScrollSlide>
+                <div className="liitems hover:animate-everScale flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-center items-center ml-[36px] ">
+                  <span className='bg-[#FF5A4D] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>14%</span>
+                  Users spending 14% more time <br className="hidden md:inline" /> on X
+                </div>
+              </RevealOnScrollSlide>
+              <RevealOnScrollSlide>
+                <div className="liitems hover:animate-everScale flex my-5 md:my-[70px] text-[25px] md:text-[44px] text-wrap tracking-tight font-medium justify-end items-center ml-[66px]">
+                  <span className='bg-[#FBE74E] w-[33px] h-[33px] md:w-[58px] md:h-[58px] flex justify-center items-center border-[1px] md:border-[2px] border-black  mx-5 px-2 py-1 rounded-[100%] text-[10px] md:text-[14px]'>No.1</span>
+                  The number 1 news channel <br className="hidden md:inline" /> on Earth
+                </div>
+              </RevealOnScrollSlide>
             </div>
 
           </section>
@@ -293,16 +339,16 @@ function App() {
               {
                 dummy.map((e, index) => (
 
-                  <div key={index} className="imagesbox basis-1/5 bg-slate-800 rounded-full overflow-hidden max-w-[33px] md:max-w-[58px] h-[33px] md:w-[58px] md:h-[58px] ">
+                  <div key={index} className="imagesbox hover:scale-110 transition-transform duration-300 ease-in-out basis-1/5 bg-slate-800 rounded-full overflow-hidden max-w-[33px] md:max-w-[58px] h-[33px] md:w-[58px] md:h-[58px] ">
                     <img src={`images/users/${e}im.svg`} alt="prof" />
                   </div>
                 ))
               }
             </div>
 
-            <button className='bg-[#4A6BFF] mt-5 border-[black] rounded-[70px] border-[2px] w-[247px] md:w-[310px] h-[54px] md:h-[66px] text-white md:text-lg text-sm '>
+            <a href='https://forms.gle/ZwiVJXzhGL2bRJNC9'><button className='bg-[#4A6BFF] hover:scale-110 transition-transform duration-300 ease-in-out mt-5 border-[black] rounded-[70px] border-[2px] w-[247px] md:w-[310px] h-[54px] md:h-[66px] text-white md:text-lg text-sm '>
               Join Now
-            </button>
+            </button></a>
 
           </section>
 
@@ -311,16 +357,16 @@ function App() {
           <section className="md:bg-[#F9FAF9] footer h-full md:h-[70vh] md:flex flex-col justify-between mt-[70px] pt-14 ">
             <div className="foot-wrap gap-[70px] mb-1 px-[25px] md:px-[110px] justify-center flex flex-col">
               <div className="straigntline flex md:flex-row flex-col justify-around gap-y-2 text-2xl font-medium">
-                <a href='https://forms.gle/ZwiVJXzhGL2bRJNC9'><div className="subtitle text-center">Apply as Creator</div></a>
-                <a href='https://forms.gle/wJNs4tyvvtVKsKjNA'><div className="subtitle text-center">Apply for Collab</div></a>
-                <a href='https://forms.gle/wJNs4tyvvtVKsKjNA'><div className="subtitle text-center">Join our Team</div></a>
-                <a href='#'><div className="subtitle text-center"> Investors Walkthrough</div></a>
+                <a href='https://forms.gle/ZwiVJXzhGL2bRJNC9' className='relative group'><div className="subtitle text-center">Apply as Creator</div>  <span className="absolute left-0 bottom-0 w-0 h-1 bg-[#303332] transition-all group-hover:w-full duration-300 ease-in-out"></span></a>
+                <a href='https://forms.gle/wJNs4tyvvtVKsKjNA' className='relative group'><div className="subtitle text-center">Apply for Collab</div>  <span className="absolute left-0 bottom-0 w-0 h-1 bg-[#303332] transition-all group-hover:w-full duration-300 ease-in-out"></span></a>
+                <a href='https://forms.gle/wJNs4tyvvtVKsKjNA' className='relative group'><div className="subtitle text-center">Join our Team</div>  <span className="absolute left-0 bottom-0 w-0 h-1 bg-[#303332] transition-all group-hover:w-full duration-300 ease-in-out"></span></a>
+                <a href='#' className='relative group'><div className="subtitle text-center"> Investors Walkthrough</div>  <span className="absolute left-0 bottom-0 w-0 h-1 bg-[#303332] transition-all group-hover:w-full duration-300 ease-in-out"></span></a>
               </div>
               <div className="straightLine items-center justify-between md:justify-center gap-y-4 flex md:flex-row md:flex-nowrap flex-wrap md:gap-x-[120px]">
-                <a href="https://x.com/xarmebot" className="socialIcon basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/Link - X → SVG.svg" alt="X (formerly Twitrter)" /></a>
-                <a href="https://x-arme-collective.gitbook.io" className="socialIcon basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/GitBook - Icon - Light 1.svg" alt="gitbook" /></a>
-                <a href="https://github.com/XARME-COLLECTIVE/" className="socialIcon basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/Link - GitHub → SVG.svg" alt="github" /></a>
-                <a href="" className="socialIcon basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/telegram.svg" alt="telegram" /></a>
+                <a href="https://x.com/xarmebot" className="socialIcon md:hover:animate-hover basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/Link - X → SVG.svg" alt="X (formerly Twitrter)" /></a>
+                <a href="https://x-arme-collective.gitbook.io" className="socialIcon md:hover:animate-hover basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/GitBook - Icon - Light 1.svg" alt="gitbook" /></a>
+                <a href="https://github.com/XARME-COLLECTIVE/" className="socialIcon md:hover:animate-hover basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/Link - GitHub → SVG.svg" alt="github" /></a>
+                <a href="" className="socialIcon md:hover:animate-hover basis-1/2 md:basis-1/4 flex justify-center items-center"><img src="images/telegram.svg" alt="telegram" /></a>
               </div>
             </div>
             <img className=' mt-16 md:mt-20 bottom-0 w-full' src="images/ARME.svg" alt="" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ export default {
   theme: {
     extend: {
       keyframes: {
+        everScale: {
+          '0%': { transform: '' },
+          '50%': { transform: 'scale(1.1)' },
+          '100%': { transform: '  ' },
+        },
+        hoverAnimation: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-6px)' }, // Adjust height as desired
+        },
+        hoverAnimationL: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-16px)' }, // Adjust height as desired
+        },
         slideIn: {
           "0%": { transform: "translateX(100%)", opacity: "0" },
           "100%": { transform: "translateX(0%)", opacity: "1" },
@@ -21,6 +34,9 @@ export default {
         scroll: "scroll 40s linear infinite",
         slideIn: "slideIn 3s ease-in-out",
         slideUp: "slideUp 3s ease-in-out",
+        hover: 'hoverAnimation 1s infinite', // Name, duration, and repetition  
+        everScale: 'everScale 2s ease-in-out infinite',
+        hoverL: 'hoverAnimationL 2s infinite ', // Name, duration, and repetition  
       },
       fontFamily: {
         grotesk: ["CabinetGrotesk", "sans-serif"],

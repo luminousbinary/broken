c104ea6000e253421e2b927bf453559909c89772
luminousbinary <boolwrld@outlook.com>
2024-11-27 11:49:40 +0000

    padding and text content hero section and text size

--- src/App.tsx
+++ src/App.tsx
@@ -96,17 +96,17 @@ function App() {
           </div>
 
         </nav>
- <RevealOnScroll>
-        <section className=" px-[25px] md:px-[110px] hero flex flex-col text-center items-center gap-[40px] justify-center md:h-screen mt-[103px] md:mt-0">
-         
+
+        <RevealOnScroll>
+          <section className=" px-[25px] md:px-[110px] hero flex flex-col text-center items-center gap-[40px] justify-center md:h-screen mt-[103px] md:mt-0">
             <div className="head md:text-2xl text-xl font-normal md:font-semibold font-inter ">X-Arme Collective</div>
-            <div className="title tracking-tighter md:text-7xl text-5xl font-semibold text-center">The Gamified Lifestyle <br className='hidden md:inline' /> and Growth SuperApp.</div>
+            <div className="title tracking-tighter md:text-8xl text-5xl font-semibold text-center">The Gamified <br className='hidden md:inline' /> Telegam SuperApp.</div>
             <div className="subtitle text-lg text-[#636363] md:text-xl font-medium text-center ">A community-oriented, utility-driven product hub <br className='hidden md:inline' /> on The Open Network.</div>
             <button className='bg-[#4A6BFF] border-[black] rounded-[70px] border-[2px] w-[247px] md:w-[310px] h-[54px] md:h-[66px] text-white md:text-lg text-sm '>Enter MotherApp<img className='inline px-2 h-2 md:h-5' src="images/inverse/telegram.svg" alt="" /></button>
-        </section>
-                 </RevealOnScroll>
- <RevealOnScroll>
-          <section className=" px-[25px] md:px-[110px] companies-starch mt-[70px]  md:mt-[-80px]">
+          </section>
+        </RevealOnScroll>
+        <RevealOnScroll>
+          <section className=" px-[25px] md:px-[110px] companies-starch mt-[70px]  md:mt-[-70px]">
             <div className="singleline md:mb-[70px] md:gap-[60px] flex items-center justify-evenly md:justify-center w-full ">
               {
                 dummy2.map((e, index) => (
